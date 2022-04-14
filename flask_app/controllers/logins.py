47c8a134df880ea2f15b5eac8826d85011462203
bree parker <breannaparker1991@gmail.com>
2022-04-14 15:16:21 -0600

    prelim

--- flask_app/controllers/logins.py
+++ flask_app/controllers/logins.py
@@ -1,3 +1,48 @@
+from pkg_resources import require
 from flask_app import app
-from flask import redirect, request, render_template, sssion
+from flask import redirect, request, render_template, session,  flash
 from flask_app.models.login import Login
+from flask_bcrypt import Bcrypt
+bcrypt = Bcrypt(app)
+
+@app.route('/')
+def index():
+  return render_template("index.html")
+
+@app.route('/register', methods=['POST'])
+def register():
+  Login.validate(request.form)
+    # validate the form here ...
+    # create the hash
+    pw_hash = bcrypt.generate_password_hash(request.form['password'])
+    print(pw_hash)
+    # put the pw_hash into the data dictionary
+    data = {
+        "first_name": request.form['first_name'],
+        "last_name": request.form['last_Name'],
+        "email": request.form['email'],
+        "password" : pw_hash
+    }
+    # Call the save @classmethod on User
+    user_id = Login.save(data)
+    # store user id into session
+    session['user_id'] = user_id
+    return redirect("/dashboard")
+  
+@app.route('/login', methods=['POST'])
+def login():
+    # see if the username provided exists in the database
+    data = { "email" : request.form["email"] }
+    user_in_db = Login.get_by_email(data)
+    # user is not registered in the db
+    if not user_in_db:
+        flash("Invalid Email/Password")
+        return redirect("/")
+    if not bcrypt.check_password_hash(user_in_db.password, request.form['password']):
+        # if we get False after checking the password
+        flash("Invalid Email/Password")
+        return redirect('/')
+    # if the passwords matched, we set the user_id into session
+    session['user_id'] = user_in_db.id
+    # never render on a post!!!
+    return redirect("/dashboard")
--- flask_app/models/login.py
+++ flask_app/models/login.py
@@ -3,6 +3,41 @@ from flask import flash
 import re
 from flask_app.config.mysqlconnection import connectToMySQL
 EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+from flask_bcrypt import Bcrypt        
 
-Class Login:
+
+# bcrypt = Bcrypt(app)   
+
+class Login:
   db = "login_registration"
+  def __init__(self,data):
+    self.id = data['id']
+    self.first_name = data['first_name']
+    self.last_name = data['last_name']
+    self.email = data['email']
+    self.password = data['password']
+    self.created_at = data['created_at']
+    self.updated_at = data['updated_at']
+    
+  
+  @classmethod
+  def save(cls,data):
+    query = "INSERT INTO users (username, password) VALUES (%(username)s, %(password)s);"
+    return connectToMySQL("mydb").mysql.query_db(query, data)
+
+
+  @classmethod
+  def get_by_email(cls,data):
+    query = "SELECT * FROM users WHERE email = %(email)s;"
+    result = connectToMySQL("mydb").query_db(query,data)
+      # Didn't find a matching user
+    if len(result) < 1:
+      return False  
+    return cls(result[0])
+
+  @staticmethod
+  def validate(user):
+    is_valid = True
+    if len(user['password']) < 7:
+      is_valid = False
+    
